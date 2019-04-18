ef704ea7e38154073d00a4dfac48888a7c3f089b
Guilherme Garber <ggarber@protonmail.com>
2019-04-17 22:00:01 -0300

    Calculos e fomatação adicionada

--- public/js/app.js
+++ public/js/app.js
@@ -22,7 +22,19 @@ function getMoth() {
     mesAtual.value = month[d.getMonth()];
 }
 
-//EVENT LISTENERS
+
+
+//Variables and Event Listeners
 const novoGasto = document.querySelector('[name=valor]');
-novoGasto.addEventListener('change',getMoth);
 const mesAtual = document.querySelector('[name=mes]');
+novoGasto.addEventListener('change',getMoth);
+
+const gastosValor = [...document.querySelectorAll('tr td.cell:first-child')];
+gastosValor.forEach(valor => valor.textContent = parseFloat(valor.textContent).toFixed(2)); //formata
+const gastoTotal = gastosValor.reduce((acc, el) => acc + parseFloat(el.textContent), 0);
+const gastoMes = {
+    total: gastoTotal,
+    disponivel: 1200 - gastoTotal
+}
+document.querySelector('#gasto-total').textContent = gastoMes.total;
+document.querySelector('#gasto-disponivel').textContent = gastoMes.disponivel;
